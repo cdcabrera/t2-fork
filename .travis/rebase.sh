a8af030a5967565b8ab153cbbf77dbb7fd5869c2
CD Cabrera <ccabrera@redhat.com>
2020-05-04 21:08:53 -0400

    chore(build): travis

---
+++ .travis/rebase.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+#
+# Clone, build for local development
+#
+gitRebase()
+{
+    local GITREMOTE=$1
+    local GITBRANCH=$2
+    local GITSOURCEBRANCH=$3
+
+    printf "${YELLOW}Attempting to rebase branch \"${GITBRANCH}\" ...${NOCOLOR}"
+
+    if [ ! -z "$(git branch -a | grep $GITBRANCH)" ]; then
+      git checkout ${GITBRANCH}
+      git rebase ${GITSOURCEBRANCH}
+      git push -f ${GITREMOTE} ${GITBRANCH} --quiet
+      printf "${GREEN}SUCCESS${NOCOLOR}\n"
+    else
+      printf "${YELLOW}SKIPPING${NOCOLOR}\n"
+    fi
+}
+#
+#
+# main()
+#
+{
+  set -e
+  set -x
+
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  NOCOLOR="\e[39m"
+
+  # filter
+  if [ "${AUTO_REBASE}" != "true" ] || [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [[ $REPO != *"$TRAVIS_REPO_SLUG"* ]]; then
+    echo -e "${YELLOW}Exiting early, rebase conditions not met.${NOCOLOR}"
+    exit 0;
+  fi
+
+  # filter process for specific branch and CI stage
+  if [[ "${TRAVIS_BRANCH}" = "${BRANCH}" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Rebase"* ]]; then
+    set +x
+    openssl aes-256-cbc \
+            -K `env | grep 'encrypted_.*_key' | cut -f2 -d '='` \
+            -iv `env | grep 'encrypted_.*_iv' | cut -f2 -d '='` \
+            -in .travis/release_key.enc -out .travis/release_key -d
+    set -x
+
+    chmod 600 .travis/release_key
+    eval `ssh-agent -s`
+    ssh-add .travis/release_key
+
+    echo -e "${YELLOW}Attempting rebase...${NOCOLOR}\n"
+
+    git config --global user.name ${USER}
+    git config --global user.email ${EMAIL}
+    git remote add ssh-origin ${REPO}
+    git checkout ${BRANCH}
+
+    gitRebase $REPO "stage" $BRANCH
+    gitRebase $REPO "qa" $BRANCH
+    gitRebase $REPO "ci" $BRANCH
+
+    echo -e "${GREEN}Rebase completed.${NOCOLOR}"
+    exit 0;
+  fi
+
+  # fallback messaging
+  echo -e "${YELLOW}Exiting, nothing to rebase${NOCOLOR}"
+  exit 0;
+}
+
+}
---
+++ .travis/release.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+#
+# main()
+#
+{
+  set -e
+  set -x
+
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  NOCOLOR="\e[39m"
+  DIFF_CHECK=$(git diff | grep '^+' | grep -v '+++' | grep -v 'Last updated' | wc -l)
+
+  # filter out pull requests
+  if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo -e "${YELLOW}Exiting early, pull request${NOCOLOR}"
+    exit 0;
+  fi
+
+  # filter out forked repositories
+  if [[ $REPO != *"$TRAVIS_REPO_SLUG"* ]]; then
+    echo -e "${YELLOW}Exiting early, not master repository${NOCOLOR}"
+    exit 0;
+  fi
+
+  # if auto deploy active filter out timestamp only updates
+  if [ "${AUTO_DEPLOY}" = "true" ] && [[ "${DIFF_CHECK}" = "0" ]]; then
+    echo -e "${YELLOW}$(git diff --compact-summary){$NOCOLOR}"
+    echo -e "${YELLOW}Exiting early, no significant changes${NOCOLOR}"
+    exit 0;
+  fi
+
+  # filter out deployment tokens when active
+  if [ "${AUTO_DEPLOY}" != "true" ] && [[ ! "$TRAVIS_COMMIT_MESSAGE" =~ "[RELEASE]" ]]; then
+    echo -e "${YELLOW}Exiting early, not a release${NOCOLOR}"
+    exit 0;
+  fi
+
+  # filter process for specific branch and CI stage
+  if [[ "${TRAVIS_BRANCH}" = "${BRANCH}" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
+    set +x
+    openssl aes-256-cbc \
+            -K `env | grep 'encrypted_.*_key' | cut -f2 -d '='` \
+            -iv `env | grep 'encrypted_.*_iv' | cut -f2 -d '='` \
+            -in .travis/release_key.enc -out .travis/release_key -d
+    set -x
+
+    chmod 600 .travis/release_key
+    eval `ssh-agent -s`
+    ssh-add .travis/release_key
+
+    echo -e "${YELLOW}PUSHING release...${NOCOLOR}"
+
+    git config --global user.name ${USER}
+    git config --global user.email ${EMAIL}
+    git remote add ssh-origin ${REPO}
+    git checkout ${BRANCH}
+
+    git fetch --tags
+    yarn release:increment
+
+    git push ssh-origin ${BRANCH} --follow-tags --quiet
+
+    echo -e "${GREEN}COMPLETED release${NOCOLOR}"
+
+    if [ "${AUTO_REBASE}" = "true" ]; then
+      git checkout stage
+      git rebase ${BRANCH}
+      git push -f origin stage --quiet
+      git checkout qa
+      git rebase ${BRANCH}
+      git push -f origin qa --quiet
+      git checkout ci
+      git rebase ${BRANCH}
+      git push -f origin ci --quiet
+    fi
+
+    exit 0;
+  fi
+
+  # fallback messaging
+  echo -e "${YELLOW}Exiting, not ${BRANCH} branch or build stage${NOCOLOR}"
+  exit 0;
+}
