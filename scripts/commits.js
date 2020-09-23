0848aa4197581ec7d8bfeddb23bd423f155e9c42
CD Cabrera <ccabrera@redhat.com>
2020-09-23 12:20:12 -0400

    chore(test): lint

---
+++ scripts/commits.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+const commits = process.argv[1];
+
+const messages = commits
+  .toString()
+  .trim()
+  .replace(/\+\s/g, '')
+  .split(/\n/g)
+  .map(message => {
+    const [hashTypeScope, ...issueNumberDescription] =
+      (/:/.test(message) && message.split(/:/)) || message.split(/\s/);
+
+    const [hash, typeScope = ''] = hashTypeScope.split(/\s/);
+    const [issueNumber, ...description] = issueNumberDescription.join(' ').trim().split(/\s/g);
+
+    const updatedTypeScope = (typeScope && `${typeScope}:`) || '';
+    const updatedDescription = description.join(' ');
+    const [
+      updatedMessage,
+      remainingMessage = ''
+    ] = `${updatedTypeScope} ${issueNumber} ${updatedDescription}`.split(/\(#\d{1,5}\)/);
+
+    return {
+      trimmedMessage:
+        (remainingMessage.trim().length === 0 && updatedMessage.trim()) ||
+        `${updatedTypeScope} ${issueNumber} ${updatedDescription}`,
+      hash,
+      typeScope: updatedTypeScope,
+      issueNumber,
+      description: updatedDescription
+    };
+  });
+
+const messagesList = messages.map(message => {
+  const {
+    trimmedMessage = null,
+    typeScope = null,
+    issueNumber = null,
+    description = null
+  } = message;
+
+  const issueNumberException =
+    /(^chore\([\d\D]+\))|(^fix\([\d\D]+\))|(^perf\([\d\D]+\))|(^build\([\d\D]+\))|(^[\d\D]+\(build\))/.test(
+      typeScope
+    ) || /\(#[\d\D]+\)$/.test(description);
+
+  const typeScopeValid =
+    (/(^[\d\D]+\([\d\D]+\):$)|(^[\d\D]+:$)/.test(typeScope) && 'valid') || 'INVALID: type scope';
+
+  const issueNumberValid =
+    (/(^issues\/[\d,]+$)/.test(issueNumber) && 'valid') ||
+    (issueNumberException && 'valid') ||
+    'INVALID: issue number';
+
+  const descriptionValid =
+    (/(^[\d\D]+$)/.test(description || (issueNumberException && issueNumber)) && 'valid') ||
+    (issueNumberException && !description && issueNumber && 'valid') ||
+    'INVALID: description';
+
+  const lengthValid =
+    (trimmedMessage && trimmedMessage.length <= 65 && 'valid') ||
+    `INVALID: message length (${trimmedMessage && trimmedMessage.length} > 65)`;
+
+  // <type>([scope]): issues/<number> <description> <messageLength>
+  return `${typeScope}<${typeScopeValid}> ${issueNumber}<${issueNumberValid}> ${description}<${descriptionValid}><${lengthValid}>`;
+});
+
+const filteredMessages = messagesList.filter(
+  value => !/<valid>[\d\D]*<valid>[\d\D]*<valid><valid>/.test(value)
+);
+
+if (filteredMessages.length) {
+  console.error(filteredMessages);
+} else {
+  console.info('Commit lint success');
+}
